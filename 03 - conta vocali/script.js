2adf32c6ac217b98780c50277811efbf90e5b511
qsrdev <matteo.albertini747@gmail.com>
2025-04-24 12:10:20 +0200

    feat: task 3 done

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,36 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+// function vowelCount(string) {
+//   const vowels = "aeiou";
+//   let contavocali = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     console.log(string[i]);
+//     if (vowels.includes(string[i])) {
+//       contavocali++;
+//     }
+//   }
+//   return contavocali;
+// }
+
+const vowelCount = (string) => {
+  const vowels = "aeiou";
+  let contavocali = 0;
+  for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+    if (vowels.includes(string[i])) {
+      contavocali++;
+    }
+  }
+  return contavocali;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let numerovocali = vowelCount(word);
+console.log(numerovocali);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
