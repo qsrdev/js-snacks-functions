791307a02953dead2d697c94db16303390588760
qsrdev <matteo.albertini747@gmail.com>
2025-04-24 12:13:19 +0200

    feat: task 4 done

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
 
+const saluta = (string) => {
+  console.log(`ciao ${string}`);
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let greet = saluta(name);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
