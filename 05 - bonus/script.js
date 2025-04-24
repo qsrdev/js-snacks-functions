3e96b74ddb2ef0ed64e3bbdeb85482a90be35583
qsrdev <matteo.albertini747@gmail.com>
2025-04-24 15:49:01 +0200

    feat: task bonus completed

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,22 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
+const date = new Date();
+console.log(date);
 
 // Dichiara la funzione qui.
-
+const hourlyGreet = (user) => {
+  let hour = date.getHours();
+  if (hour <= 13) {
+    console.log(`buongiorno ${user}`);
+  } else if (hour <= 17) {
+    console.log(`buon pomeriggio ${user}`);
+  } else if (hour > 17) {
+    console.log(`buonasera ${user}`);
+  }
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+let greet = hourlyGreet(name);
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
