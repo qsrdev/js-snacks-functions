9bde994152663da8059fd3a19f7150bab2f34dcf
qsrdev <matteo.albertini747@gmail.com>
2025-04-24 11:47:49 +0200

    feat: es 2 done

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,23 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+// const letter = prompt("Dammi la tua letter: A")
+let letter = "a";
 
 // Dichiara la funzione qui.
-
+function sortby(array, startletter) {
+  let initialarray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][0].toUpperCase() === startletter.toUpperCase()) {
+      initialarray.push(array[i]);
+    }
+  }
+  return initialarray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let sortedarray = sortby(names, letter);
+console.log(sortedarray);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+// TI DEVI RICORDARE CHE QUANDO FAI RICERCA SULLE STRINGHE è IMPORTANTE LA CAPITALIZZAZIONE DELLE LETTERE
