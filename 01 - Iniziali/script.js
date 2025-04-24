39336ea13b7bedbd451ca1665a3b32a1bc50b31d
qsrdev <matteo.albertini747@gmail.com>
2025-04-24 11:32:19 +0200

    feat: task1 done

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,18 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function firstletter(array) {
+  let namesInitial = [];
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    namesInitial.push(array[i][0]);
+  }
+  return namesInitial;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let primeletter = firstletter(names);
+console.log(primeletter);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
